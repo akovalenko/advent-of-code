ba1215e741e3c882901eaaadf9861d6dbd6e1211
Anton Kovalenko <anton@sw4me.com>
2022-12-06 08:32:48 +0300

    add faster implementation to #6

--- 2022/06.lisp
+++ 2022/06.lisp
@@ -20,3 +20,30 @@
 
 (defun part-2 (&optional (data (parse-input)))
   (find-marker data 14))
+
+;;; alternative
+(defun find-marker-fast (data group-size)
+  (when (<= group-size (length data))
+    (let ((histogram (make-array 128 :initial-element 0))
+	  (ones 0))
+      (flet ((register (char)
+	       (case (incf (aref histogram (char-code char)))
+		 (1 (incf ones))
+		 (2 (decf ones))))
+	     (forget (char)
+	       (case (decf (aref histogram (char-code char)))
+		 (1 (incf ones))
+		 (0 (decf ones)))))
+	(dotimes (i group-size)
+	  (register (char data i)))
+	(loop
+	  for start from 0
+	  for end from group-size
+	  do (when (= ones group-size)
+	       (return end))
+	     (when (= end (length data))
+	       (return nil))
+	     (forget (char data start))
+	     (register (char data end)))))))
+
+
