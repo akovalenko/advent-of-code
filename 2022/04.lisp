e2c772a0ab7e9c1289f56a0e36b90d0bce197e9c
Anton Kovalenko <anton@sw4me.com>
2022-12-18 10:03:02 +0300

    nicer comparison

--- 2022/04.lisp
+++ 2022/04.lisp
@@ -15,10 +15,8 @@
 
 (defun part-1 (&optional (data (parse-input)))
   (loop for ((a1 b1) (a2 b2)) in data
-	count (or (and (<= a2 a1 b2)
-		       (<= a2 b1 b2))
-		  (and (<= a1 a2 b1)
-		       (<= a1 b2 b1)))))
+	count  (or (<= a2 a1 b1 b2)
+		   (<= a1 a2 b2 b1))))
 
 (defun part-2 (&optional (data (parse-input)))
   (loop for ((a1 b1) (a2 b2)) in data
